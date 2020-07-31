8177e26374b6b15f0344f47c6257034d163088db
Basmatz <59979218+Basmatz@users.noreply.github.com>
2020-07-31 13:52:32 +0200

    update

--- Week_03/Bike_Demand.py
+++ Week_03/Bike_Demand.py
@@ -1,126 +1,216 @@
+#%%
+
+###Imports
 import seaborn as sns
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import RobustScaler
 import time
+from sklearn.preprocessing import StandardScaler
 
-pd.set_option('display.width', 300)
-pd.set_option('display.max_columns',20)
+#Plot Size
+plt.rcParams["figure.figsize"] = (14, 8)
 
-train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
+#%%
 
+###Load Data
+df = pd.read_csv("train.csv")
+df_test = pd.read_csv("test.csv")
 
-X = train.drop(["count","casual","registered"], axis=1)
-y = train["count"]
+#%% md
+
+### Train Test Split
+
+#%%
+
+X = df.drop(["count","casual","registered"], axis=1)
+y = df["count"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
+
+#%% md
+
+### Data Exploration
+
+#%%
+
+eda = X_train.join(y_train)
+
+#%%
+
+eda.isna().any().sum()
+
+#%%
 
 #Transform date column to datetime format
-train["datetime"] = pd.to_datetime(train["datetime"])
+eda["datetime"] = pd.to_datetime(eda["datetime"])
 
-cats = [#"datetime",
-        "season",
-        "holiday",
-        "workingday",
-        "weather",
-        "temp",
-        "atemp",
-        "humidity",
-        "windspeed",
-        "weekend"
+
+#%%
+
+#Create time columns
+eda["hour"] = eda["datetime"].dt.hour
+eda["day"] = eda["datetime"].dt.day
+eda["month"] = eda["datetime"].dt.month
+eda["year"] = eda["datetime"].dt.year
+eda["yday"] = eda["datetime"].dt.dayofyear
+eda["wday"] = eda["datetime"].dt.dayofweek
+
+#%%
+
+#Create column for weekend
+eda.loc[(eda["datetime"].apply(lambda x: x.weekday() > 4)), "weekend"] = 1
+eda.loc[(eda["datetime"].apply(lambda x: x.weekday() <= 4)), "weekend"] = 0
+
+
+#%%
+
+#Difference between holiday, weekday and weekend
+
+sns.lineplot(x=eda["hour"],y=eda["count"],hue=eda["holiday"])
+plt.show()
+sns.lineplot(x=eda["hour"],y=eda["count"],hue=eda["workingday"])
+plt.show()
+sns.lineplot(x=eda["hour"],y=eda["count"],hue=eda["weekend"])
+
+# daytype = train[(train["holiday"] == 0)
+#     & (train["workingday"] == 0)
+#                     ]
+# daytype
+
+#%%
+
+#Overall Average
+overall_mean = int(round(eda["count"].mean()))
+print(f"Average Usage overall: {overall_mean}")
+
+#Average usage on holidays
+holiday_mean = int(round(eda["count"].loc[(eda["holiday"] == 1)].mean()))
+print(f"Average Usage on holidays: {holiday_mean}")
+
+noholiday_mean = int(round(eda["count"].loc[(eda["holiday"] == 0)].mean()))
+print(f"Average Usage on non-holidays: {noholiday_mean}")
+
+#Average usage on weekends
+weekend_mean = int(round(eda["count"].loc[(eda["weekend"] == 1)].mean()))
+print(f"Average Usage on weekends: {weekend_mean}")
+
+workingday_mean = int(round(eda["count"].loc[(eda["workingday"] == 1)].mean()))
+print(f"Average Usage on working days: {workingday_mean}")
+
+
+#%%
+
+
+#Usage per weekday through the seasons
+heat = pd.pivot_table(data=eda,
+                    index='season',
+                    values='count',
+                    columns='wday',
+                    aggfunc=np.mean)
+
+heat
+sns.heatmap(data=heat,cmap='coolwarm', annot=True, fmt=".1f", annot_kws={'size':16})
+
+
+
+#%%
+
+#Correlations
+sns.heatmap(eda.corr(), annot=True)
+
+#%%
+
+#Pairplot
+# pair = eda[eda["month"] == 6]
+# sns.pairplot(pair,
+#         palette="husl",
+#         plot_kws={'line_kws':{'color':'red'}},
+#         kind="reg")
+
+#%% md
+
+### Feature Engineering
+
+#%%
+
+#Relevant Categories
+cats = [
+    "season",
+    "weather",
+    "temp",
+    "atemp",
+    "humidity",
+    "hour",
+    "month",
+    "year",
+    "yday"
         ]
 
-train["hour"] = train["datetime"].dt.hour
-train["day"] = train["datetime"].dt.day
-train["month"] = train["datetime"].dt.month
-train["year"] = train["datetime"].dt.year
-train["yday"] = train["datetime"].dt.dayofyear
+#%%
+
+#PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 
 
-df2 = train[(train["holiday"] == 1)
-    & (train["year"] == 2012)
-                    ]
-
-counti = train["count"].loc[
-    (train["holiday"] == 1)
-    & (train["year"] == 2011)
-                    ].mean()
-
-print(counti)
-
-#fig, axs = plt.subplots()
-#fig = sns.regplot(x="hour",y="count",data=df)
-#fig.set(ylim=(-20,900),xlim=(-5,30))
-
-df = train[
-            (train["season"] == 1)
-            & (train["year"] == 2012)
-            #& (train["yday"].isin(range(100,300)))
+features = [
+    "temp",
+    "yday"
         ]
 
-counti = train["count"].loc[
-            (train["holiday"] == 0)
-            & (train["year"] == 2011)
-            & (train["yday"].isin(range(100,300)))
-        ].mean()
-
-print(counti)
-print(df)
-
-#fig2 = sns.regplot(x="hour",y="count",data=df)
-#fig2.set(ylim=(-20,900),xlim=(-5,30))
-
-sns.pairplot(df[["casual",
-        "registered",
-        "temp",
-        "humidity",
-        "windspeed",
-        "holiday"
-        ]], palette="husl", kind="reg")
-
+sns.scatterplot(eda[features[0]],eda[features[1]])
 plt.show()
 
+X_train = eda[features]
+X_train
+
+X_train = StandardScaler().fit_transform(X_train)
+
+pca = PCA(n_components=2)
+pca.fit_transform(X_train)
+
+print(pca.components_)
+
+print(pca.explained_variance_)
+
+
+def draw_vector(v0, v1, ax=None):
+    ax = ax or plt.gca()
+    arrowprops=dict(arrowstyle='->',
+                    linewidth=2,
+                    shrinkA=0, shrinkB=0)
+    ax.annotate('', v1, v0, arrowprops=arrowprops)
+
+# plot data
+plt.scatter(X_train[:, 0], X_train[:, 1], alpha=0.2)
+for length, vector in zip(pca.explained_variance_, pca.components_):
+    v = vector * 3 * np.sqrt(length)
+    draw_vector(pca.mean_, pca.mean_ + v)
+
+plt.axis('equal')
 
 
 
+#%% md
+
+### Train a model
+
+#%%
 
 
 
+m = LinearRegression()
+
+m.fit(X_train,y_train)
+
+print(m.score(X_train,y_train))
+
+#%%
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# X_train, X_test, y_train, y_test = train_test_split(X[cats], y, train_size=0.8, test_size=0.2, random_state=0)
-#
-# m = LinearRegression()
-#
-# m.fit(X_train,y_train)
-#
-# print(m.score(X_train,y_train))
--- Week_03/Course.py
+++ Week_03/Course.py
@@ -64,40 +64,40 @@ print(scaled2)
 
 
 
+<
 
+degrees = [2,5,9]
 
-# degrees = [2,5,9]
-#
-# def true_fun(X):
-#     return np.cos(1.5 * np.pi * X)
-#
-# for i in degrees:
-#     ax = plt.subplot(1, len(degrees), i + 1)
-#     plt.setp(ax, xticks=(), yticks=())
-#
-#     # Make a pipeline
-#     polynomial_features = PolynomialFeatures(degree=degrees[i],
-#                                              include_bias=True)
-#     linear_regression = LinearRegression()
-#     pipeline = Pipeline([("polynomial_features", polynomial_features),
-#                          ("linear_regression", linear_regression)])
-#     pipeline.fit(x[:, np.newaxis], y)
-#
-#     # Evaluate the models using crossvalidation
-#     scores = cross_val_score(pipeline, x[:, np.newaxis], y,
-#                              scoring="neg_mean_squared_error", cv=10)
-#
-#     # Actually plot the function
-#     X_test = np.linspace(0, 1, 100)
-#     plt.plot(X_test, pipeline.predict(X_test[:, np.newaxis]), label="Model")
-#     plt.plot(X_test, true_fun(X_test), label="True function")
-#     plt.scatter(X, y, edgecolor='b', s=20, label="Samples")
-#     plt.xlabel("x")
-#     plt.ylabel("y")
-#     plt.xlim((0, 1))
-#     plt.ylim((-2, 2))
-#     plt.legend(loc="best")
-#     plt.title("Degree {}\nMSE = {:.2e}(+/- {:.2e})".format(
-#         degrees[i], -scores.mean(), scores.std()))
-#
-# plt.show()
+def true_fun(X):
+    return np.cos(1.5 * np.pi * X)
+
+for i in degrees:
+    ax = plt.subplot(1, len(degrees), i + 1)
+    plt.setp(ax, xticks=(), yticks=())
+
+    # Make a pipeline
+    polynomial_features = PolynomialFeatures(degree=degrees[i],
+                                             include_bias=True)
+    linear_regression = LinearRegression()
+    pipeline = Pipeline([("polynomial_features", polynomial_features),
+                         ("linear_regression", linear_regression)])
+    pipeline.fit(x[:, np.newaxis], y)
+
+    # Evaluate the models using crossvalidation
+    scores = cross_val_score(pipeline, x[:, np.newaxis], y,
+                             scoring="neg_mean_squared_error", cv=10)
+
+    # Actually plot the function
+    X_test = np.linspace(0, 1, 100)
+    plt.plot(X_test, pipeline.predict(X_test[:, np.newaxis]), label="Model")
+    plt.plot(X_test, true_fun(X_test), label="True function")
+    plt.scatter(X, y, edgecolor='b', s=20, label="Samples")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.xlim((0, 1))
+    plt.ylim((-2, 2))
+    plt.legend(loc="best")
+    plt.title("Degree {}\nMSE = {:.2e}(+/- {:.2e})".format(
+        degrees[i], -scores.mean(), scores.std()))
+
+plt.show()>
