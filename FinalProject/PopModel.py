7616a5e9e5087d4bdc0bd2fbc8d61ebec9d0f314
Basmatz <59979218+Basmatz@users.noreply.github.com>
2020-08-12 17:45:11 +0200

    efrh

--- FinalProject/PopModel.py
+++ FinalProject/PopModel.py
@@ -1,15 +1,56 @@
-# SET SPOTIPY_CLIENT_ID = 76e34e7d6b9449899623db078669c2be
-# SET SPOTIPY_CLIENT_SECRET= f79c6f2fdc5545cb90d377f2498498e8
+SPOTIPY_CLIENT_ID = "76e34e7d6b9449899623db078669c2be"
+SPOTIPY_CLIENT_SECRET= "f79c6f2fdc5545cb90d377f2498498e8"
 #
 # SET SPOTIPY_CLIENT_ID = 76e34e7d6b9449899623db078669c2be
 # SET SPOTIPY_CLIENT_SECRET= f79c6f2fdc5545cb90d377f2498498e8
 
-# shows artist info for a URN or URL
+#util.prompt_for_user_token(username,scope,client_id='76e34e7d6b9449899623db078669c2be',client_secret='f79c6f2fdc5545cb90d377f2498498e8',redirect_uri='http://localhost/')
+
+# # shows artist info for a URN or URL
+#
+# import spotipy
+# from spotipy.oauth2 import SpotifyOAuth
+#
+# sp = spotipy.Spotify(auth_manager=SpotifyOAuth())
+#
+# response = sp.new_releases()
+#
+# while response:
+#     albums = response['albums']
+#     for i, item in enumerate(albums['items']):
+#         print(albums['offset'] + i, item['name'])
+#
+#     if albums['next']:
+#         response = sp.next(albums)
+#     else:
+#         response = None
+
 
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+cid = "76e34e7d6b9449899623db078669c2be"
+secret = "f79c6f2fdc5545cb90d377f2498498e8"
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth())
+# artist_name = []
+# track_name = []
+# popularity = []
+# track_id = []
+#
+# for i in range(0,10000,50):
+#     track_results = sp.search(q='tag:new', type='track', limit=50,offset=i)
+#     for i, t in enumerate(track_results['tracks']['items']):
+#         artist_name.append(t['artists'][0]['name'])
+#         track_name.append(t['name'])
+#         track_id.append(t['id'])
+#         popularity.append(t['popularity'])
+#         print("Artist: " + t['artists'][0]['name'])
+#         print("Track Name: " + t['name'])
+#         print("Popularity: " + str(t['popularity']))
+#
+# # shows artist info for a URN or URL
+
 
 response = sp.new_releases()
 
