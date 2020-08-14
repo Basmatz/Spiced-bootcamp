db737d3dd87848f253e9dc5fd4f934ac04581975
Lara Ehrenhofer <lara.ehrenhofer@gmail.com>
2020-08-14 18:14:37 +0200

    weekly project update

--- Week_5/Arctic_Model.py
+++ Week_5/Arctic_Model.py
@@ -1,42 +1,96 @@
-### ARIMA model for 6 Arctic weather stations on Spitsbergen/Svalbard
+### ARIMA model for Arctic weather stations on Spitsbergen/Svalbard
 
 
 
 # imports
 import pandas as pd
 import seaborn as sns
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import date, timedelta
 from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.api import OLS, add_constant
+from statsmodels.tsa.arima_model import ARIMA
 
 # get data
 
-svalbard_stations = ['000186', '002755', '018777', '019036', '018099', '019003', '018145', '018181', '002756']
-svalbard_names = ['Svalbard Airport', 'Isford Radio', 'Barentsburg', 'Green Harbour', 'Pyramiden', 'Longyearbyen', 'Adventdalen', 'Sveagruva', 'Sveagruva II']
-
-# took out: 'Janssonhaugen Vest','Reindalspasset',
-
-svalbard_dict = dict(zip(svalbard_stations, svalbard_names))
-
-type(svalbard_stations[0])
+all_stations = pd.read_csv('all_stations_latlon_converted.csv')
 
 
-def get_data(staid):
-    filename = 'all_weather_data/TG_STAID' + str(staid) + '.txt'
-    data = pd.read_csv(filename, sep = ',', skiprows = 20)
+def find_stations(lat_min, lat_max, lon_min, lon_max):
+    '''
+    Returns weather information for all stations within a certain min/max range for latitude and longitude.
+    Takes integers.
+    Based on csv with all stations' latitude and longitude converted into decimal.
+    '''
+    stations = all_stations[all_stations['LAT_dec'] > lat_min]
+    stations = stations[stations['LAT_dec'] < lat_max]
+    stations = stations[stations['LON_dec'] > lon_min]
+    stations = stations[stations['LON_dec'] < lon_max]
+    return stations[['STAID', 'STANAME']]
+
+
+# Svalbard archipelago is between 74°-81° N, 10°-35°E
+
+all_svalbard_stations = find_stations(74, 81, 10, 35)
+
+len(all_svalbard_stations)
+
+svalbard_staids = list(all_svalbard_stations['STAID'])
+svalbard_names = list(all_svalbard_stations['STANAME'].str.strip())
+
+# convert staids to strings:
+def station_id_to_char(id):
+    '''
+    Converts int station IDs into 6 digit string, with remainder filled with 0
+    '''
+    len_zeros = 6 - len(str(id))
+    id_char = (str(0) * len_zeros) + str(id)
+    return id_char
+
+svalbard_staid_chars = []
+for id in svalbard_staids:
+    station_id_char = station_id_to_char(id)
+    svalbard_staid_chars.append(station_id_char)
+
+svalbard_dict = dict(zip(svalbard_staid_chars, svalbard_names))
+
+files = [f for f in os.listdir('all_weather_data/') if not f.startswith('.') and f.startswith('TG_STAID')]
+
+def get_data(staid, filename):
+    '''
+    Get data for a specific weather station ID and filename
+    '''
+    data = pd.read_csv('all_weather_data/' + filename, sep = ',', skiprows = 20)
     return data
 
 svalbard = []
+missing_data_staid = []
 
-for staid in svalbard_stations:
-    data = get_data(staid)
-    data['STANAME'] = svalbard_dict[staid]
-    svalbard.append(data)
+for staid in svalbard_staid_chars:
+    filename = 'TG_STAID' + str(staid) + '.txt'
+    print(filename)
+    if filename in files:
+        print(f'found filename {filename}')
+        dat = get_data(staid, filename)
+        dat['STANAME'] = svalbard_dict[staid]
+        svalbard.append(dat)
+    else:
+        print(f'no data available for station {staid}')
+        missing_data_staid.append(staid)
+
+
+# take the stations we have no temp data for out of the list of stations
+missing_data_staname = [svalbard_dict[x] for x in missing_data_staid]
+
+svalbard_names_stripped = [x for x in svalbard_names if x not in missing_data_staname]
 
 svalbard = pd.concat(svalbard)
+# svalbard = svalbard.reset_index()
 
 svalbard.columns = svalbard.columns.map(str.strip)
+
 # svalbard.columns
 
 svalbard['DATE'] = pd.to_datetime(svalbard['DATE'], format = '%Y%m%d')
@@ -47,23 +101,23 @@ svalbard['DAY'] = pd.to_datetime(svalbard['DATE']).dt.day
 svalbard['YEAR_WEEK'] = svalbard['YEAR'].astype(str) + ' ' + svalbard['WEEK'].astype(str)
 svalbard['YEAR_MONTH'] = svalbard['YEAR'].astype(str) + ' ' + svalbard['MONTH'].astype(str)
 
-len(svalbard)
-
 # find missing values everywhere
 missing = svalbard.groupby(['STANAME'])['Q_TG'].value_counts()
-
 missing
 
 # plot missing values -- I want to see a) when each weather station is active, b) whether they are all missing data on the same days or if there is hope of using group averages from that day
 
 def plot_q_tg(station):
+    '''
+    Plot missing values in separate plot for each station
+    '''
     sns.lineplot(x = 'DATE', y = 'Q_TG', data = svalbard.loc[svalbard['STANAME'] == station])
     plt.xlim(svalbard['DATE'].min(), svalbard['DATE'].max())
     plt.title(str(station))
     plt.show()
     plt.close()
 
-for station in svalbard_names:
+for station in svalbard_names_stripped:
     plot_q_tg(station)
 
 
@@ -74,109 +128,108 @@ for station in svalbard_names:
 svalbard = svalbard[svalbard.Q_TG != 9]
 
 # check again:
-for station in svalbard_names:
-    plot_q_tg(station)
+# for station in svalbard_names:
+#     plot_q_tg(station)
 
 # for each year check balance of no. lines contributed by each station... leave this for later too
 
-# average by week and month for each weather station
-svalbard.columns
 
-svalbard['weekly_mean'] = svalbard.groupby('YEAR_WEEK')['TG'].transform('mean')
-svalbard['monthly_mean'] = svalbard.groupby('YEAR_MONTH')['TG'].transform('mean')
+###### TEST-TRAIN SPLIT.
+###### since we are getting into averaging, etc. territory...
+
+y_train = svalbard[svalbard['DATE'] <= pd.to_datetime('1999-12-31')]
+y_test = svalbard[svalbard['DATE'] > pd.to_datetime('1999-12-31')]
+
+# average by week and month for each weather station; transform by mean.
+
+y_train['weekly_mean'] = y_train.groupby(['YEAR_WEEK', 'STAID'])['TG'].transform('mean')
+y_train['monthly_mean'] = y_train.groupby(['YEAR_MONTH', 'STAID'])['TG'].transform('mean')
 
 # Check for trends, seasons etc. -- do I need to de-trend etc. before building a model?
-print(plot_acf(svalbard['weekly_mean'], lags=100))
+print(plot_acf(y_train['weekly_mean'], lags=100))
 
 # looks like there is a slight trend -- check seasonality
-print(plot_acf(svalbard['monthly_mean'], lags=100))
+print(plot_acf(y_train['monthly_mean'], lags=100))
 
 
 # looks like there is v little seasonality (maybe not surprising? it is always cold on Svalbard...)
 # but it can't hurt to both de-season and de-trend the data.
-
-# first do a train-test split: training = first 20 days of each month, test = rest
-# y_train = svalbard[svalbard.DAY < 20]
-# y_test = svalbard[svalbard.DAY < 20]
-
-y_train = svalbard[svalbard['DATE'] < pd.to_datetime('2020-05-31')]
-y_test = svalbard[svalbard['DATE'] > pd.to_datetime('2020-05-31')]
-
-# Need to re-do the average temps to avoid test-train contamination.
-
-y_train['weekly_mean'] = y_train.groupby(['YEAR_WEEK', 'STANAME'])['TG'].transform('mean')
-y_train['monthly_mean'] = y_train.groupby(['YEAR_MONTH', 'STANAME'])['TG'].transform('mean')
-
-y_test['weekly_mean'] = y_test.groupby(['YEAR_WEEK', 'STANAME'])['TG'].transform('mean')
-y_test['monthly_mean'] = y_test.groupby(['YEAR_MONTH', 'STANAME'])['TG'].transform('mean')
-
-y_train.STANAME.value_counts()
 
 # some plotting of the temps
 
 plt.figure(figsize = (20,8))
 sns.lineplot(x = 'DATE', y = 'weekly_mean', data = y_train, hue = 'STANAME', alpha = .3)
 
-# find the gaps in different stations' dataset
-
-longyearbyen = svalbard[svalbard['STANAME'] == 'Longyearbyen']
-
-longyearbyen.iloc[3000]
-
-longyearbyen.iloc[3012]
-
-longyearbyen.iloc[3013]
 
 
+# DE-TREND AND DE-SEASON DATA.
+# there is a strong seasonal effect, as we see in the plot above
+# we saw from the autocorrelation plot that there is also a trends
+
+# first, determine whether the trend is linear or quadratic
+
+# add timestep: it needs to align by datetime, rather than by the index.
+
+# len(y_train)
+min_date = y_train['DATE'].min()
+max_date = y_train['DATE'].max()
+
+def get_timestep(date):
+    timestep = date - min_date # timestep is a Timedelta object
+    return timestep.days
+
+# make linear timestep
+y_train['TIMESTEP'] = y_train['DATE'].apply(get_timestep)
+
+# plot just to check.
+plt.plot(y_train['DATE'], y_train['TIMESTEP'])
+
+# make quadratic TIMESTEP
+y_train['TIMESTEP_sq'] = y_train['TIMESTEP']**2
+plt.plot(y_train['DATE'], y_train['TIMESTEP_sq'])
 
 
-deltas = longyearbyen['DATE'].diff()
+# check if linear or quadratic timestep works better.
+X = add_constant(y_train['TIMESTEP'])
 
-deltas.to_csv('deltas.csv')
+X_sq = add_constant(y_train[['TIMESTEP', 'TIMESTEP_sq']])
 
+# y_train.columns
 
-longyearbyen[1910:1925]
-
-
-
-# Filter diffs (here days > 1, but could be seconds, hours, etc)
-gaps = deltas[deltas > timedelta(days=1)]
-
-gaps
+m = OLS(y_train['monthly_mean'], X)
+results = m.fit()
+results.summary()
 
 
-for i, g in gaps.iteritems():
-    print(i)
-    print(g)
-    # gap_start = longyearbyen['DATE'][i - 1]
-    # print(f'Start: {datetime.strftime(gap_start, "%Y-%m-%d")} | '
-    #       f'Duration: {str(g.to_pytimedelta())}')
+m_sq = OLS(y_train['monthly_mean'], X_sq)
+results_sq = m_sq.fit()
+results_sq.summary()
 
-len(gaps)
+# conclusion: need to use quadratic timestep
 
-gaps
+## now actually de-trend & de-season
+list_of_weather_data = list(y_train.groupby(['STAID']))
 
-gaps2 = gaps.reset_index()
-gaps2.columns = ['index','gap size']
+for i, station in list_of_weather_data:
+    station['weekly_pct_change'] = station['weekly_mean'].pct_change()
 
-index = 1919
+dataframes = [station for i, station in list_of_weather_data]
 
-longyearbyen.iloc[index]
+y_train2 = pd.concat(dataframes)
 
-longyearbyen.iloc[index+1]
 
-longyearbyen.iloc[index+2]
+# quick plots
 
-longyearbyen.iloc[index-1]
+y_train2['weekly_pct_change'].plot()
 
-gap_info = []
-for index in gaps2['index']:
-    # gap_info.append(svalbard.loc[row])
 
-gaps2
+#### train ARIMA model
+y_train2.columns
 
-gaps
-
+m_ar = ARIMA(list(y_train2['weekly_mean']), (1, 2, 2)) # (ar, d, ma) <- Order of the ARIMA model
+m_ar.initialize()
+results = m_ar.fit()
+results.summary()
 
 
 
